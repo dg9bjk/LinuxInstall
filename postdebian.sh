6352483d52d0a076722e1ba2c5ba3c80c1f162a6
root <root@Linux-AST.blackhole.lan>
2021-01-14 14:04:06 +0100

    neue Datei:     postdebian.sh 	neue Datei:     postraspberry.sh

---
+++ postdebian.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+#sed
+#
+apt-get check
+apt-get update
+apt-get upgrade
+apt-get dist-upgrade
+#
+apt-get install joe
+apt-get install mc
+apt-get install psmisc
+apt-get install tcpdump
+apt-get install gcc
+apt-get install g++
+apt-get install gdb
+apt-get install strace
+apt-get install wireshark
+apt-get install make
+apt-get install automake
+apt-get install cmake
+apt-get install linux-headers-4*
+apt-get install ffmpeg
+apt-get install ssh
+apt-get install ftp
+apt-get install ncftp
+apt-get install filezilla
+apt-get install nmap
+apt-get install git
+#
